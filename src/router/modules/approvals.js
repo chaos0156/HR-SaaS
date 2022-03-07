77858c121c6529c551edae8c9647e9f422d5ce98
chaos0156 <429018715@qq.com>
2022-03-07 15:30:10 +0800

    完成路由页面的整理

---
+++ src/router/modules/approvals.js
@@ -0,0 +1,14 @@
+import Layout from '@/layout'
+export default {
+  path: '/approvals',
+  name: 'approvals',
+  component: Layout,
+  children: [{
+    path: '',
+    component: () => import('@/views/approvals'),
+    meta: {
+      title: '审批',
+      icon: 'tree-table'
+    }
+  }]
+}
